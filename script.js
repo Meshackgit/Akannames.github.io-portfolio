ffdb1f8e860a71b049ceb51e5021bee056f3ec80
Meshackgit <meshmwangi828@gmail.com>
2021-12-10 08:21:07 +0300

    some changes

---
+++ script.js
@@ -0,0 +1,5 @@
+var CC,YY,MM,DD,mod;
+const maleNames =["Kwaku","Kwadmo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+const femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+const daysValues = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const getName = () => '${maleNames[math.floor(math.random() * maleNames.length)]} ${femaleNames[math.floor(math.random() * femaleNames.length)]}';
